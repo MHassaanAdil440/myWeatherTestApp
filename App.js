49d3dd30d0967b43ac44402b8e7bdbfb41198c52
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-06-02 17:55:48 +0500

    error Geo location not supported

--- App.js
+++ App.js
@@ -3,11 +3,35 @@ import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import DateTime from './component/DateTime';
 import { styles } from './styles';
 import WeatherScroll from './component/WeatherScroll';
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const img = require('./assets/bgimg5.jpg');
 
 export default function App() {
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((success) => {
+        let { latitude, longitude } = success.coords;
+        fetchDataFromApi(latitude, longitude);
+        console.log(latitude, longitude);
+        console.log(success);
+      });
+    } else {
+      console.log('Geolocation is not supported');
+    }
+  }, []);
+
+  const fetchDataFromApi = (latitude, longitude) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=49cc8c821cd2aff9af04c9f98c36eb74`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        showWeatherData(data);
+      });
+  };
+
   return (
     <View style={styles.containerApp}>
       <ImageBackground source={img} style={styles.bgimgApp}>
@@ -18,4 +42,3 @@ export default function App() {
     </View>
   );
 }
-
